1318cc6303d71a355a4a760110c33931a54e2c19
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-06 12:29:42 +0100

    Finished intermediate Exercise 2

--- java.js
+++ java.js
@@ -13,47 +13,57 @@ console.log(max);
 
 // Intermediate Exercise 1
 
-let students = ['Martin', 'Thomas', 'Klaus', 'Maria', 'David'];
-let grades = [76, 85, 65, 93, 81];
+// let students = ['Martin', 'Thomas', 'Klaus', 'Maria', 'David'];
+// let grades = [76, 85, 65, 93, 81];
 
-function averagePoints () {
-    let sum = 0;
-    for (let i = 0; i < grades.length; i++) {
-        sum = sum + grades[i];
-    }
-    return result=(sum/grades.length);
-}
+// function averagePoints () {
+//     let sum = 0;
+//     for (let i = 0; i < grades.length; i++) {
+//         sum = sum + grades[i];
+//     }
+//     return result=(sum/grades.length);
+// }
 
-console.log(averagePoints());
+// console.log(averagePoints());
 
-for (let i = 0; i < grades.length; i++) {
-    if (grades[i] < 60) {
-        console.log(students[i] + ' got an F');
-    } else if (grades[i] < 70) {
-        console.log(students[i] + ' got a D');
-    } else if (grades[i] < 80) {
-        console.log(students[i] + ' got a C');
-    } else if (grades[i] < 90) {
-        console.log(students[i] + ' got a B');
-    } else {
-        console.log(students[i] + ' got an A');
-    }
-}
+// for (let i = 0; i < grades.length; i++) {
+//     if (grades[i] < 60) {
+//         console.log(students[i] + ' got an F');
+//     } else if (grades[i] < 70) {
+//         console.log(students[i] + ' got a D');
+//     } else if (grades[i] < 80) {
+//         console.log(students[i] + ' got a C');
+//     } else if (grades[i] < 90) {
+//         console.log(students[i] + ' got a B');
+//     } else {
+//         console.log(students[i] + ' got an A');
+//     }
+// }
 
-function classGrade() {
-    if (averagePoints() < 60) {
-        console.log('The class got an F');
-    } else if (averagePoints() < 70) {
-        console.log('The class got a D');
-    } else if (averagePoints() < 80) {
-        console.log('The class got a C');
-    } else if (averagePoints() < 90) {
-        console.log('The class got a B');
-    } else {
-        console.log('The class got an A');
-    }
-}
-classGrade();
+// function classGrade() {
+//     if (averagePoints() < 60) {
+//         console.log('The class got an F');
+//     } else if (averagePoints() < 70) {
+//         console.log('The class got a D');
+//     } else if (averagePoints() < 80) {
+//         console.log('The class got a C');
+//     } else if (averagePoints() < 90) {
+//         console.log('The class got a B');
+//     } else {
+//         console.log('The class got an A');
+//     }
+// }
+// classGrade();
 
 // Intermediate Exercise 2
+
+for (i = 1; i <= 100; i++) {
+    if (Number.isInteger(i/15)) {
+        console.log('FizzBuzz');
+    } else if (Number.isInteger(i/5)) {
+        console.log('Buzz');
+    } else if (Number.isInteger(i/3)) {
+        console.log('Fizz');
+    } else {console.log(i)}
+}
 
